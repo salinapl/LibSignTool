055f871fb2a1bcea49e8c0633ee4a5629f74235c
Swizzler121 <36831696+Swizzler121@users.noreply.github.com>
2025-07-19 16:46:00 -0500

    Continue plugin restructure
    Continuing to convert functions to work in the plugin format vs a normal site.

--- index.php
+++ index.php
@@ -1,18 +1,20 @@
 <?php
 
+use Kirby\Toolkit\Str;
+
 Kirby::plugin('salinapl/libsigntool', [
     'blueprints' => [
-        'fields/links' => __DIR__ . '/blueprints/links.yml',
+        'fields/lst-links' => __DIR__ . '/blueprints/links.yml',
         'files/image' => __DIR__ . '/blueprints/image.yml',
         'files/video' => __DIR__ . '/blueprints/video.yml',
-        'pages/slideshows' => __DIR__ . '/blueprints/slideshows.yml',
-        'pages/slideshow' => __DIR__ . '/blueprints/slideshow.yml',
+        'pages/lst-slideshows' => __DIR__ . '/blueprints/slideshows.yml',
+        'pages/lst-slideshow' => __DIR__ . '/blueprints/slideshow.yml',
         'pages/web-slides' => __DIR__ . '/blueprints/goal.yml',
         'pages/web-slides' => __DIR__ . '/blueprints/goal2.yml',
         'pages/web-slides' => __DIR__ . '/blueprints/goal.yml',
         'pages/web-slides' => __DIR__ . '/blueprints/events.yml',
         'pages/web-slides' => __DIR__ . '/blueprints/error-slide.yml',
-        'pages/galleries' => __DIR__ . '/blueprints/gallery.yml',
+        'pages/lst-gallery' => __DIR__ . '/blueprints/gallery.yml',
         'pages/videogalleries' => __DIR__ . '/blueprints/videogallery.yml'
 
     ],
@@ -20,19 +22,69 @@ Kirby::plugin('salinapl/libsigntool', [
         'slideshow' => require __DIR__ . '/controllers/slideshow.php'
     ],
     'templates' => [
-        'error-slide' => __DIR__ . '/templates/error-slide.php',
-        'events' => __DIR__ . '/templates/events.php',
-        'goal' => __DIR__ . '/templates/goal.php',
-        'goal2' => __DIR__ . '/templates/goal2.php',
-        'opac' => __DIR__ . '/templates/opac.php',
-        'slideshow' => __DIR__ . '/templates/slideshow.php',
-        'slideshows' => __DIR__ . '/templates/slideshows.php'
+        'lst-error-slide' => __DIR__ . '/templates/error-slide.php',
+        'lst-events' => __DIR__ . '/templates/events.php',
+        'lst-goal' => __DIR__ . '/templates/goal.php',
+        'lst-goal2' => __DIR__ . '/templates/goal2.php',
+        'lst-opac' => __DIR__ . '/templates/opac.php',
+        'lst-slideshow' => __DIR__ . '/templates/slideshow.php',
+        'lst-slideshows' => __DIR__ . '/templates/slideshows.php'
     ],
     'snippets' => [
-        'builder' => __DIR__ . '/snippets/builder.php',
-        'event' => __DIR__ . '/snippets/event.php',
-        'footer' => __DIR__ . '/snippets/footer.php',
-        'header' => __DIR__ . '/snippets/header.php'
+        'lst-builder' => __DIR__ . '/snippets/builder.php',
+        'lst-event' => __DIR__ . '/snippets/event.php',
+        'lst-footer' => __DIR__ . '/snippets/footer.php',
+        'lst-header' => __DIR__ . '/snippets/header.php'
+    ],
+    'options' => [
+        // checks for landscape or portrait tag in URL
+        'routes' => [
+            [
+            'pattern' => 'slideshows/(:any)/(:any)',
+            'action' => function ($subpage, $orientation) {
+                $data = [
+                    'orientation' => $orientation,
+                ];
+                return page('slideshows/' . $subpage)?->render($data);
+            }
+            ]
+        ]
+    ],
+    'fields' => [
+        'slideshowSelect' => [
+            'extends' => 'select',
+            'props' => [
+                // Kirby calls this to build your <select> options
+                'options' => function (): array {
+                    $page = site()->find('slideshows');
+                    return $page
+                        ? $page->children()->pluck('title', 'id')
+                        : [];
+                }
+            ]
+        ]
+    ],
+    'hooks' => [
+        // fires after plugins are registered
+        'system.loadPlugins:after' => function () {
+            $kirby = kirby();
+            $page = 'slideshows';
+            if ($kirby->page($page)?->exists()) {
+                return;
+            }
+
+            // create the page
+            $kirby->impersonate(
+                'kirby',
+                fn () => $kirby->site()->createChild([
+                    'slug' => $page,
+                    'template' => 'slideshows',
+                    'content' => [
+                        'uuid' => $page,
+                    ]
+                ])->changeStatus('unlisted')
+            );
+        }
     ]
         // plugin magic happens here
 ]);
