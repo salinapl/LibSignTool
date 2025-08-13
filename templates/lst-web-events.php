9758b96b24bcfb0158b1d0a356317ba40587dcb1
Swizzler121 <36831696+Swizzler121@users.noreply.github.com>
2025-08-13 18:22:09 -0500

    Update lst-web-events.php

--- templates/lst-web-events.php
+++ templates/lst-web-events.php
@@ -3,7 +3,7 @@
         <?= css('/media/plugins/salinapl/libsigntool/css/templates/event.css') ?>
     <?php endslot() ?>
     <?php slot() ?>
-        <h1><?= $page->heading() ?></h1>
+        <h1><?= $page->headline() ?></h1>
         <?php snippet('lst-web-event-php') ?>
     <?php endslot() ?>
 <?php endsnippet() ?>
